d517dfabcba32a365bed37bd4e4815ae372c1405
lianyu <1319764245@qq.com>
2026-01-30 14:26:01 +0800

    feat: 使用统一返回对象返回接口结果

--- src/main/java/com/yu/apiinterface/controller/UserController.java
+++ src/main/java/com/yu/apiinterface/controller/UserController.java
@@ -2,6 +2,10 @@ package com.yu.apiinterface.controller;
 
 import cn.hutool.core.codec.Base64;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yu.apiinterface.common.BaseResponse;
+import com.yu.apiinterface.common.ErrorCode;
+import com.yu.apiinterface.common.ResultUtils;
+import com.yu.apiinterface.exception.ApiException;
 import com.yu.apiinterface.model.entity.User;
 import com.yu.apiinterface.service.UserService;
 import com.yu.yuapiclientsdk.utils.SignUtils;
@@ -27,21 +31,21 @@ public class UserController {
     private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping("/getNameById")
-    public String getNameById(Long id) {
+    public BaseResponse<String> getNameById(Long id) {
         User user = userService.getById(id);
         if (user == null) {
-            throw new RuntimeException("用户不存在");
+            throw new ApiException(ErrorCode.NOT_FOUND_ERROR, "用户不存在");
         }
-        return "GET 名字是" + user.getUserName();
+        return ResultUtils.success("GET 名字是" + user.getUserName());
     }
 
     @PostMapping("/getName")
-    public String getNameByPost(@RequestParam String name) {
-        return "Post 你的名字是" + name;
+    public BaseResponse<String> getNameByPost(@RequestParam String name) {
+        return ResultUtils.success("Post 你的名字是" + name);
     }
 
     @PostMapping("/getUserName")
-    public String getUserNameByPost(@RequestBody User user, HttpServletRequest request) {
+    public BaseResponse<String> getUserNameByPost(@RequestBody User user, HttpServletRequest request) {
         String accessKey = request.getHeader("accessKey");
         String timestamp = request.getHeader("timestamp");
         String nonceStr = request.getHeader("nonceStr");
@@ -51,7 +55,7 @@ public class UserController {
         // 从数据库中查询secretKey
         User existUser = userService.getOne(new QueryWrapper<User>().eq("accessKey", accessKey));
         if (existUser == null) {
-            throw new RuntimeException("无权限");
+            throw new ApiException(ErrorCode.NO_AUTH_ERROR, "无权限");
         }
         // 构造需要参与签名的参数
         String secretKey = existUser.getSecretKey();
@@ -63,20 +67,20 @@ public class UserController {
         // 重新生成签名
         String twiceSignature = SignUtils.signature(timestamp, accessKey, secretKey, nonceStr, rawBody, paramsMap);
         if (!twiceSignature.equals(signature)) {
-            throw new RuntimeException("签名错误");
+            throw new ApiException(ErrorCode.SIGN_ERROR, "签名错误");
         }
         // 请求 5 分钟有效期
         if (System.currentTimeMillis() - Long.parseLong(timestamp) > 5 * 1000 * 60) {
-            throw new RuntimeException("请求过期");
+            throw new ApiException(ErrorCode.EXPIRED_ERROR, "请求过期");
         }
         // 从 redis 中获取随机数，如果 5 分钟内获取到相同随机数，则校验不通过
         String nonceStrKey = "api:nonceStr:" + accessKey + ":" + nonceStr;
         String nonceStrCache = stringRedisTemplate.opsForValue().get(nonceStrKey);
         if (nonceStr.equals(nonceStrCache)) {
-            throw new RuntimeException("重复请求");
+            throw new ApiException(ErrorCode.REPEAT_ERROR, "重复请求");
         } else {
             stringRedisTemplate.opsForValue().set(nonceStrKey, nonceStr, 5 * 60, TimeUnit.SECONDS);
         }
-        return "Post 用户名字是" + user.getUserName();
+        return ResultUtils.success("Post 用户名字是" + user.getUserName());
     }
 }
---
+++ src/main/java/com/yu/apiinterface/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.yu.apiinterface.exception;
+
+import com.yu.apiinterface.common.BaseResponse;
+import com.yu.apiinterface.common.ErrorCode;
+import com.yu.apiinterface.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    public BaseResponse<?> businessExceptionHandler(ApiException e) {
+        log.error("ApiException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
